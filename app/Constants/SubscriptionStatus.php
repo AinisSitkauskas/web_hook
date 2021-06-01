18f8d4414d857cea2920922993e83eb3b2ad1b3b
AinisSitkauskas <ainis795@gmail.com>
2021-06-01 17:50:19 +0300

    Added data subscription statuses

---
+++ app/Constants/SubscriptionStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+class SubscriptionStatus
+{
+    public const CREATE = 'create';
+
+    public const UPDATE = 'update';
+
+    public const CANCEL = 'cancel';
+}
